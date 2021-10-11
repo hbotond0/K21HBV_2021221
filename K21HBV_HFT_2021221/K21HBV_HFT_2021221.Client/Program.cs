904c8ff359e1e472ab476536c76d7a762b9e2288
hbotond0 <horvathbotond0@gmail.com>
2021-10-11 14:51:55 +0200

    Dependencies

---
+++ K21HBV_HFT_2021221/K21HBV_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace K21HBV_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
