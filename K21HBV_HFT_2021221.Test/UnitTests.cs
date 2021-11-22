8e539684329b8af23238b4b78ee600931721ea11
bglrk <>
2021-11-22 23:51:48 +0100

    Update UnitTests.cs

--- K21HBV_HFT_2021221.Test/UnitTests.cs
+++ K21HBV_HFT_2021221.Test/UnitTests.cs
@@ -20,6 +20,8 @@ namespace K21HBV_HFT_2021221.Test
             Mock<ICustomers> mockedCustRepo = new Mock<ICustomers>(MockBehavior.Loose);
             CustomerLogic custLogic = new CustomerLogic(mockedCustRepo.Object);
             mockedCustRepo.Setup(repo => repo.AddNew(It.IsAny<Customers>()));
+            custLogic.AddNewCustomer("Willy Wonka", 50);
+            mockedCustRepo.Verify(repo => repo.AddNew(It.IsAny<Customers>()), Times.Once);
         }
 
         [Test]
@@ -28,6 +30,8 @@ namespace K21HBV_HFT_2021221.Test
             Mock<IProstitutes> mockedProstiRepo = new Mock<IProstitutes>(MockBehavior.Loose);
             ProstituteLogic prostiLogic = new ProstituteLogic(mockedProstiRepo.Object);
             mockedProstiRepo.Setup(repo => repo.AddNew(It.IsAny<Prostitutes>()));
+            prostiLogic.AddNewProsti("Minnie", "latina", 120, 19, false);
+            mockedProstiRepo.Verify(repo => repo.AddNew(It.IsAny<Prostitutes>()), Times.Once);
         }
 
         [Test]
@@ -36,17 +40,36 @@ namespace K21HBV_HFT_2021221.Test
             Mock<IPimps> mockedPimpRepo = new Mock<IPimps>(MockBehavior.Loose);
             PimpLogic pimpLogic = new PimpLogic(mockedPimpRepo.Object);
             mockedPimpRepo.Setup(repo => repo.AddNew(It.IsAny<Pimps>()));
+            pimpLogic.AddNewPimp("Maffia Boss", 10);
+            mockedPimpRepo.Verify(repo => repo.AddNew(It.IsAny<Pimps>()), Times.Once);
         }
 
         [Test]
         public void TestProstiSTDChange()
         {
+            Mock<IProstitutes> mockedProstiRepo = new Mock<IProstitutes>(MockBehavior.Loose);
+
+            List<Prostitutes> testgirls = new List<Prostitutes>()
+            {
+                new Prostitutes(){ Id = 1, Name = "First Girl", Age = 21, Category = "blonde", Price = 69, STDs = true},
+                new Prostitutes(){ Id = 2, Name = "Second Girl", Age = 43, Category = "milf", Price = 200, STDs = false},
+            };
+
+            mockedProstiRepo.Setup(repo => repo.ListAll()).Returns(testgirls.AsQueryable());
+            mockedProstiRepo.Setup(x => x.ListOne(It.IsAny<int>())).Returns((int i) => testgirls.Where(x => x.Id == i).Single());
+
+            ProstituteLogic prostiLogic = new ProstituteLogic(mockedProstiRepo.Object);
+
+            prostiLogic.UpdateProstHasSTD(1, false);
+            mockedProstiRepo.Verify(repo => repo.UpdateHasSTD(1, false), Times.Once);
 
         }
 
         [Test]
         public void TestDeletePimp()
         {
+            Mock<IPimps> mockedPimpRepo = new Mock<IPimps>(MockBehavior.Loose);
+
 
         }
 
