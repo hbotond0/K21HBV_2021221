e2ae315495fe1d89f159bfb55b1d2c4c6f0bb94c
bglrk <>
2021-11-22 23:18:46 +0100

    Update ICustomerLogic.cs

--- K21HBV_HFT_2021221.Logic/ICustomerLogic.cs
+++ K21HBV_HFT_2021221.Logic/ICustomerLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using K21HBV_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace K21HBV_HFT_2021221.Logic
 {
     public interface ICustomerLogic
     {
+        Customers GetOneCustomer(int id);
+
+        IEnumerable<Customers> GetAllCustomers();
+
+        Customers AddNewCustomer(string name, int age);
+
+        void ChangeName(int id, string newName);
+
+
     }
 }
